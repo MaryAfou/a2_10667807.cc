013ef532c1c6c925059f8cb0b8a6d3e75fc4c56c
MaryAfou <42815307+MaryAfou@users.noreply.github.com>
2018-09-03 00:31:19 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade;
+    int mark;
+
+
+
+cout<<"Welcome to ug grading system"<<endl;
+
+cout<<"What is your mark"<<endl;
+cin>>mark;
+switch(mark){
+       case 80 ... 100:
+    cout<<"grade A"<<endl;
+    break;
+       case 75  ...  79:
+        cout<<"grade B+"<<endl;
+        break;
+
+
+
+
+}
+
+    return 0;
+}
